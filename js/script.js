f5f0f7473e210a718c1fecd7db16985c926b500c
Aromal Anil <aromalanilkannan@gmail.com>
2020-02-11 00:24:23 +0530

    home section completed

--- js/script.js
+++ js/script.js
@@ -4,8 +4,6 @@
 
  $(document).ready(()=>{
 
-    console.log('done');
-    
     $(window).scroll(function() {
         if ($(window).scrollTop() > 10) {
             $('.nav-bar').addClass('floatingNav');
@@ -13,4 +11,35 @@
             $('.nav-bar').removeClass('floatingNav');
         }
     });
+
+    $('.ham').click(function(){
+		$('.nav-elements').toggleClass('nav-visible');
+	});
+
+    const b=baffle(".title");
+    b.set({
+      characters : '█<▓ ░█▒█▒ ░▒/░█ ▓▒▓ █/██▒ █▓█/ █▒█ ░█▒█ ▓░█▒',
+      speed: 120
+    });
+
+
+    var i=1;
+    let changeTitle = () =>
+    {
+      let titles= ["Web Developer","UI/UX Designer","Android Developer","Programmer"];
+      if(i==titles.length)
+      {
+          i=0;
+      }
+      b.text(currentText => titles[i]).reveal(1500);
+      i++;
+    }
+
+    let interval = setInterval(changeTitle, 3000);
+    
+
+
+      
+
  });
+
