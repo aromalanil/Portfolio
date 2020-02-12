69280296c93767a60c53c302cda37a21d3bd9617
Aromal Anil <aromalanilkannan@gmail.com>
2020-02-12 22:43:51 +0530

    fixed main section

--- js/script.js
+++ js/script.js
@@ -2,41 +2,57 @@
  * @author Aromal Anil <aromalanilkannan@gmail.com>
  */
 
- $(document).ready(()=>{
+$(document).ready(() => {
 
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > 10) {
-            $('.nav-bar').addClass('floatingNav');
-        } else {
-            $('.nav-bar').removeClass('floatingNav');
-        }
-    });
-
-    $('.ham').click(function(){
-		$('.nav-elements').toggleClass('nav-visible');
-	});
-
-    const b=baffle(".title");
-    b.set({
-      characters : '█<▓ ░█▒█▒ ░▒/░█ ▓▒▓ █/██▒ █▓█/ █▒█ ░█▒█ ▓░█▒',
-      speed: 120
-    });
+  //NavBar Drop Shadow on scroll
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 10) {
+      $('.nav-bar').addClass('floatingNav');
+    } else {
+      $('.nav-bar').removeClass('floatingNav');
+    }
+  });
 
 
-    var i=1;
-    let changeTitle = () =>
-    {
-      let titles= ["Web Developer","UI/UX Designer","Android Developer","Programmer"];
-      if(i==titles.length)
-      {
-          i=0;
-      }
-      b.text(currentText => titles[i]).reveal(1500);
-      i++;
+  // Menu bar for Mobile
+  $('.ham').click(function () {
+
+    //Making Side Menu Bar Visible/Invisible On Menu Click
+    $('.nav-elements').toggleClass('nav-visible');
+
+
+    //Making Side Menu Invisible when clicked on any link/btn in menu
+    if ($('.nav-elements').hasClass('nav-visible')) {
+      $('.nav-link').click(() => {
+        $('.ham').click();
+      })
+      $('.nav-btn').click(() => {
+        $('.ham').click();
+      })
     }
 
-    let interval = setInterval(changeTitle, 3000);
-    
+  });
 
- });
+
+  //Changing Title Effect
+  const b = baffle(".title");
+  b.set({
+    characters: '█<▓ ░█▒█▒ ░▒/░█ ▓▒▓ █/██▒ █▓█/ █▒█ ░█▒█ ▓░█▒',
+    speed: 120
+  });
+
+  var i = 1;
+  let changeTitle = () => {
+    let titles = ["Web Developer", "UI/UX Designer", "Android Developer", "Programmer"];
+    if (i == titles.length) {
+      i = 0;
+    }
+    b.text(currentText => titles[i]).reveal(1500);
+    i++;
+  }
+
+  let interval = setInterval(changeTitle, 3000);
+
+
+});
 
